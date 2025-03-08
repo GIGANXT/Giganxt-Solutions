8bf554fda3253e3fbe72c0ec0e657cbd37374a78
SUMIT0110 <122148407+SUMIT0110@users.noreply.github.com>
2025-03-08 15:26:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    cssCodeSplit: false,
-    cssMinify: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  css: {
-    postcss: './postcss.config.js',
-    devSourcemap: true,
-  }
+  plugins: [react()]
 });
