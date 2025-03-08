671ff028467a79f439b0b0a3c2d3957278f8875a
SUMIT0110 <122148407+SUMIT0110@users.noreply.github.com>
2025-03-08 15:13:56 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
 export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {
-      overrideBrowserslist: ['last 2 versions', '> 1%', 'IE 11', 'not dead'],
-      grid: true,
-      flexbox: true
-    },
+    autoprefixer: {},
   },
 }
